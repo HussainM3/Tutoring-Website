7da92e605ea940c45f59b66d391790b3b380b39e
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-02 23:46:13 -0500

    - added css file for MathUnit components - styled the components and headers

--- tutoring-site/src/components/MathUnit.js
+++ tutoring-site/src/components/MathUnit.js
@@ -12,7 +12,7 @@ function MathUnit(props){
     return(
         // display info on the units
         <div>
-            <h2>{props.grade} Grade: {props.title}</h2>
+            <h2> {props.grade}: {props.title}</h2>
             {/* Will add more content specific to the unit here */}
         </div>
     );
