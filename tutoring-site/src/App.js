2480bcc200b1369e6bcf745b4c2475661092cfaf
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-02 22:54:08 -0500

    - displaying a math unit component

--- tutoring-site/src/App.js
+++ tutoring-site/src/App.js
@@ -2,12 +2,15 @@
 import React from 'react';
 
 // import components
-import Header from './components/Header'; 
+import Header from './components/Header';
+import MathUnit from './components/MathUnit';
 
 function App() {
   return (
     <div>
       <Header />
+      {/* Display example math unit */}
+      <MathUnit grade = "Grade 3" title = "Intro to Multiplication" />
       {/* components will continue to go here */}
     </div>
   );
