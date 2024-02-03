06b94c4f4193e5620c47727a1302a81fd4add0b3
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-02 23:33:48 -0500

    - made array of math units

--- tutoring-site/src/App.js
+++ tutoring-site/src/App.js
@@ -6,6 +6,13 @@ import Header from './components/Header';
 import MathUnit from './components/MathUnit';
 
 function App() {
+  // sample array of math units
+  const mathUnits = [
+    {grade: "Grade 3", title: "Introduction to Multiplication"},
+    {grade: "Grade 3", title: "1-digit Multiplication"},
+    {grade: "Grade 4", title: "Place Value"},
+  ]
+
   return (
     <div>
       <Header />
