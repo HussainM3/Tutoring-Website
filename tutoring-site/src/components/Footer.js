c0ee8121ec862e8cfbaba0f133c581c1dc1c5ca3
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-03 00:28:46 -0500

    - added footer js file

---
+++ tutoring-site/src/components/Footer.js
@@ -0,0 +1,14 @@
+/* JS file for footer of the website */
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    // display footer
+    return(
+        <div className="footer">
+            <p>&copy; 2024 Math Tutoring Service</p>
+        </div>
+    )
+}
+
+export default Footer;
