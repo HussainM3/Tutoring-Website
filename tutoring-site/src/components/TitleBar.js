0d924abcc561a960c6dc007ebc3924d607b0c961
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-02 23:56:33 -0500

    - added title bar js and css file

---
+++ tutoring-site/src/components/TitleBar.js
@@ -0,0 +1,14 @@
+/* JS file for titlebar */
+
+import React from "react";
+import "./TitleBar.css";
+
+function TitleBar(){
+    return(
+        <div className="title-bar">
+            <h1>Math Tutoring Website</h1>
+        </div>
+    );
+}
+
+export default TitleBar;
