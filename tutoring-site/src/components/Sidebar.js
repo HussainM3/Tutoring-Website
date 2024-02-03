a9fae6de7f0beb4648ae3bf704887125ae18a073
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-03 00:01:42 -0500

    - created sidebar js and css file

---
+++ tutoring-site/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+/* JS file for sidebars */
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar(){
+    return(
+        <div className="side-bar">
+            {/* All sidebar content will go here */}
+            <p>Grade 3</p>
+            <p>Grade 4</p>
+            <p>Grade 5</p>
+        </div>
+    )
+}
+
+export default Sidebar;
