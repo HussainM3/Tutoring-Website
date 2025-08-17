652b16f87c6a82359f069296c44b9c3bfb662cf1
Hussain Muhammed <muhammeh@mcmaster.ca>
2025-08-17 19:43:10 -0400

    starting html for main page

---
+++ tutoring site/index.php
@@ -0,0 +1,46 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Cover Page</title>
+		<link rel="stylesheet" type="text/css" href="/shared/css/styles.css" />
+	</head>
+	<body>
+		<!-- <header class="site-header">
+			<div class="left">
+				<a href="index.php">
+					<img src="/shared/images/icon.png" alt="Site Icon" class="logo">
+				</a>
+			</div>
+		</header>             -->
+		<nav class="main-nav">
+			<div class="menu-item"><a href="index.php">Home</a></div>
+			<div class="menu-item"><a href="contact.php">Contact</a></div>
+		</nav>
+		
+		<div class="content-section">
+			<div class="information">
+				<img id="part1-logo" src="shared/images/tutor-logo.png">
+				<div class="information-text">
+					<h1 id="title">Tutoring Service</h1>
+
+				</div>
+			</div>
+			<div class="information">
+				<div class="information-text">
+					
+				</div>
+			</div>
+		</div>
+		
+		<!-- <footer class="site-footer">
+			<div class="left">
+				<a href="/TMA2/tma2.htm" class="cover-link">Home</a>
+			</div>
+		</footer>  -->
+
+		<script src="/shared/js/main.js"></script>
+	</body>
+</html>
